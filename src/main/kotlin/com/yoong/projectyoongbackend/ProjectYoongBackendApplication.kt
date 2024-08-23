e0fc72b79fff158ee208981d9c46fea630246fad
Ppajingae <wkmkyj991112@gmail.com>
2024-08-23 18:51:50 +0900

    ✨ First Project Commit

---
+++ src/main/kotlin/com/yoong/projectyoongbackend/ProjectYoongBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.yoong.projectyoongbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectYoongBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectYoongBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/yoong/projectyoongbackend/ProjectYoongBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoong.projectyoongbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectYoongBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
