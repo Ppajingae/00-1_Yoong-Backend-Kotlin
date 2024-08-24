4f60252e5bba99b62d33c1a7f666fe613f366792
Ppajingae <wkmkyj991112@gmail.com>
2024-08-24 17:50:18 +0900

    ✨Feat Implement temp Member Repository

--- src/main/kotlin/com/yoong/projectyoongbackend/domain/auth/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/yoong/projectyoongbackend/domain/auth/member/repository/MemberRepository.kt
@@ -2,9 +2,13 @@ package com.yoong.projectyoongbackend.domain.auth.member.repository
 
 import com.yoong.projectyoongbackend.domain.auth.member.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
 
 interface MemberRepository {
+
     fun save(member: Member): Member
+
+    fun findByIdOrNull(id: Long): Member?
 }
 
 interface MemberJpaRepository : JpaRepository<Member, Long>
@@ -13,7 +17,7 @@ class MemberRepositoryImpl(
     private val memberJpaRepository: MemberJpaRepository
 ): MemberRepository{
 
-    override fun save(member: Member): Member {
-        return memberJpaRepository.save(member)
-    }
+    override fun save(member: Member): Member = memberJpaRepository.save(member)
+
+    override fun findByIdOrNull(id: Long): Member? = memberJpaRepository.findByIdOrNull(id)
 }
