d92bf2f075a02d529687173a1ecebf78cd8ac4a2
Ppajingae <wkmkyj991112@gmail.com>
2024-08-24 17:35:49 +0900

    ✨Feat Implement temp Member Service

--- src/main/kotlin/com/yoong/projectyoongbackend/domain/auth/member/service/MemberService.kt
+++ src/main/kotlin/com/yoong/projectyoongbackend/domain/auth/member/service/MemberService.kt
@@ -1,15 +1,18 @@
 package com.yoong.projectyoongbackend.domain.auth.member.service
 
+import com.yoong.projectyoongbackend.common.dto.DefaultResponse
+import com.yoong.projectyoongbackend.domain.auth.member.dto.CreateMemberDto
+import com.yoong.projectyoongbackend.domain.auth.member.dto.MemberLoginDto
 import org.springframework.stereotype.Service
 
 @Service
 class MemberService {
     
-    fun signUp(createMemberDto: CreateMemberDto): DefaultResponse? {
+    fun signUp(createMemberDto: CreateMemberDto): DefaultResponse {
 
     }
 
-    fun login(memberLoginDto: MemberLoginDto): DefaultResponse? {
+    fun login(memberLoginDto: MemberLoginDto): DefaultResponse {
 
     }
 }
